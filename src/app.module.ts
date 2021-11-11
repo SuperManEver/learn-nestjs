21ee31017efbb4ceff7f1d8c4dc741d4a4e2971c
Nikita Luparev <nikita.luparev@gmail.com>
2021-11-12 04:13:17 +0500

    some progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/controller';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ArticleModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { AppModule } from './app.module';
 
 config();
 
+const { PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT || 3000);
 }
 
 bootstrap();
