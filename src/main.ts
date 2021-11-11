4dec9a7feea8afc607d3e9c3363ce13ec8159546
Nikita Luparev <nikita.luparev@gmail.com>
2021-11-11 21:06:26 +0500

    tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import dotenv from 'dotenv';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
